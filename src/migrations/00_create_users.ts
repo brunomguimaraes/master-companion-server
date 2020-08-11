ef184ae55179be387372645f0690d3e760fc5fa6
Bruno <mguimaraesbruno@gmail.com>
2020-08-11 14:33:57 -0300

    Fix some table props names

--- src/migrations/00_create_users.ts
+++ src/migrations/00_create_users.ts
@@ -4,12 +4,10 @@ import * as Knex from 'knex'
 export async function up(knex: Knex): Promise<any> {
   return knex.schema.createTable('users',(table:Knex.CreateTableBuilder)=>{
     table.increments('id')
-    table.string('full_name',36)
-    table.timestamps(true,true)
-
+    table.string('full_name', 36)
+    table.timestamps(true, true)
   })
 }
-
 
 export async function down(knex: Knex): Promise<any> {
   return knex.schema.dropTable('users')
--- src/migrations/01_create_characters.ts
+++ src/migrations/01_create_characters.ts
@@ -5,12 +5,11 @@ export async function up(knex: Knex): Promise<any> {
   return knex.schema.createTable('characters', (table: Knex.CreateTableBuilder) => {
     table.increments('id')
     table.string('name')
-    table.integer('owner_id').references('users.id').onDelete('CASCADE')
+    table.integer('creator_id').references('users.id').onDelete('CASCADE')
     table.string('specie')
     table.timestamps(true, true)
   })
 }
-
 
 export async function down(knex: Knex): Promise<any> {
   return knex.schema.dropTable('characters')
